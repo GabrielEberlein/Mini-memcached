349fdb38f8cafdfebdf07b17c424aab107abecdb
Santiagoco1 <cosantiago2019@gmail.com>
2023-07-17 22:02:38 -0300

    Erlang Client First Attempt

--- client.erl
+++ client.erl
@@ -1,45 +1,65 @@
 -module(client).
--export([start/1, text_client/1, bin_client/1]).
--export([put/3, del/2, get/2, stats/1]).
+-export([start/1, put/3, del/2, get/2, stats/1]).
+-export([text_client/1, bin_client/1]).
+-export([task_to_text/1, parse_text/1, get_length/1, task_to_bin/1, parse_bin/1]).
 
-start(ServerSocket) ->
-    {ok, Socket} = gen_tcp:connect("localhost", ServerSocket, [{active, true}]),
-    case ServerSocket of
-        8888 ->
-            ClientPid = spawn(text_client(Socket));
-        8889 ->  
-            ClientPid = spawn(bin_client(Socket))
-    end,
-    {ok, ClientPid}.
+-define(TextPort, 8888).
+-define(BinPort, 8889). 
 
-text_client(Socket) ->
+start(ServerPort) ->
+    {ok, ServerSocket} = gen_tcp:connect("localhost", ServerPort, [{active, true}]),
+    case ServerPort of
+        ?TextPort ->
+            ClientPid = spawn(text_client(ServerSocket)),
+            {ok, ClientPid};
+        ?BinPort ->  
+            ClientPid = bin,
+            {ok, ClientPid};
+        _ ->
+            {error, "Unspecified Port"}
+    end.
+
+task_to_text(Task) ->
+    case lists:head(Task) of
+        put ->
+            io_lib:format("PUT ~p ~p", [lists:nth(2, Task), lists:nth(3, Task)]);
+        del ->
+            io_lib:format("DEL ~p", [lists:nth(2, Task)]);
+        get ->
+            io_lib:format("GET ~p", [lists:nth(2, Task)]);
+        stats ->
+            io_lib:format("STATS")
+    end.
+
+parse_text(Reply) ->
+    Chunks = string:tokens(Reply, [$\s]),
+    case lists:head(Chunks) of
+        "OK" -> 
+            case length(Chunks) of
+                1 -> ok;
+                _ -> {ok, lists:nthtail(1, Chunks)}
+            end;
+        "EINVAL" -> einval;
+        "ENOTFOUND" -> enotfound;
+        "EBINARY" -> ebinary;
+        "EBIG" -> ebig;
+        "EUNK" -> eunk
+    end.
+
+text_client(ServerSocket) ->
     receive
-        {put, K, V, Pid} ->
-            Message = io_lib:format("PUT ~p ~p", [K, V]);
-        {del, K, Pid} ->
-            Message = io_lib:format("DEL ~p", [K]);
-        {get, K, Pid} ->
-            Message = io_lib:format("GET ~p", [K]);
-        {stats, Pid} ->
-            Message = io_lib:format("STATS")
+        {Task, Pid} ->
+            Msg = task_to_text(Task),
+            gen_tcp:send(ServerSocket, Msg),
+            receive 
+                {tcp, ServerSocket, TextReply} -> 
+                    Reply = parse_text(TextReply),
+                    Pid ! Reply
+            end;
+        _ ->
+            {error, ""}
     end,
-    gen_tcp:send(Socket, Message),
-    receive 
-        {tcp, Socket, Msg} ->
-            Chunks = string:tokens(Msg, [$\s]),
-            case lists:head(Chunks) of
-                "OK" -> 
-                    case length(Chunks) of
-                        1 -> PID ! ok;
-                        _ -> PID ! {ok, lists:nthtail(1, Chunks)}
-                    end;
-                "ENOTFOUND" -> 
-                    PID ! enotfound;
-                "EINVAL" ->
-                    PID ! einval
-            end
-        end
-    text_client(Socket).
+    text_client(ServerSocket).
 
 get_length(Length) ->
     if
@@ -49,91 +69,92 @@ get_length(Length) ->
             Length
     end.
 
-bin_client(Socket) ->
+task_to_bin(Task) ->
+    case lists:head(Task) of
+        put ->
+            BinTask = integer_to_list(11),
+            LengthKey = get_length(length(lists:nth(2, Task))),
+            LengthValue = get_length(length(lists:nth(3, Task))),
+            list_to_binary(BinTask++LengthKey++(lists:nth(2, Task))++LengthValue++(lists:nth(3, Task)));
+        del ->
+            BinTask = integer_to_list(12),
+            LengthKey = get_length(length(lists:nth(2, Task))),
+            list_to_binary(BinTask++LengthKey++(lists:nth(2, Task)));
+        get ->
+            BinTask = integer_to_list(13),
+            LengthKey = get_length(length(lists:nth(2, Task))),
+            list_to_binary(BinTask++LengthKey++(lists:nth(2, Task)));
+        stats ->
+            BinTask = integer_to_list(21),
+            list_to_binary(BinTask)
+    end.
+
+separate([]) -> [];
+separate(List) ->
+    Length = list_to_integer(lists:nth(1, List)) * math:pow(256, 3) +
+             list_to_integer(lists:nth(2, List)) * math:pow(256, 2) +
+             list_to_integer(lists:nth(3, List)) * math:pow(256, 1) + 
+             list_to_integer(lists:nth(4, List)) * math:pow(256, 0),
+    String = lists:sublist(5, Length),
+    String++(lists:nthtail(Length+1, List)).
+
+parse_bin(Reply) ->
+    Chunks = binary_to_list(Reply),
+    case lists:head(Chunks) of
+        "101" -> 
+            case length(Chunks) of
+                1 -> ok;
+                _ -> 
+                    {ok, separate(lists:nthtail(1, Chunks))}
+            end;
+        "111" -> einval;
+        "112" -> enotfound;
+        "113" -> ebinary;
+        "114" -> ebig;
+        "115" -> eunk
+    end.
+
+
+bin_client(ServerSocket) ->
     receive
-        {put, K, V, Pid} ->
-            Task = integer_to_list(11),
-            LengthKey = get_length(length(K)),
-            LengthValue = get_length(length(V)),
-            Message = list_to_binary(Task++LengthKey++K++LengthValue++V);
-        {del, K, Pid} ->
-            Task = integer_to_list(12),
-            LengthKey = get_length(length(K)),
-            Message = list_to_binary(Task++LengthKey++K);
-        {get, K, Pid} ->
-            Task = integer_to_list(13),
-            LengthKey = get_length(length(K)),
-            Message = list_to_binary(Task++LengthKey++K);
-        {stats, Pid} ->
-            Task = integer_to_list(21),
-            Message = list_to_binary(Task)
+        {Task, Pid} ->
+            Msg = task_to_bin(Task),
+            gen_tcp:send(ServerSocket, Msg),
+            receive 
+                {tcp, ServerSocket, BinReply} -> 
+                    Reply = parse_bin(BinReply),
+                    Pid ! Reply
+            end;
+        _ ->
+            {error, ""}
     end,
-    gen_tcp:send(Socket, Message),
-    receive 
-        {tcp, Socket, Msg} ->
-            Chunks = string:tokens(Msg, [$\s]),
-            case lists:head(Chunks) of
-                "OK" -> 
-                    case length(Chunks) of
-                        1 -> PID ! ok;
-                        _ -> PID ! {ok, lists:nthtail(1, Chunks)}
-                    end;
-                "ENOTFOUND" -> 
-                    PID ! enotfound;
-                "EINVAL" ->
-                    PID ! einval
-            end
-        end
-    bin_client(Socket).
+    text_client(ServerSocket).
 
-put(ClientPid, K, V) ->
-    ClientPid ! {put, K, V, self()},
+put(ClientPid, Key, Value) ->
+    ClientPid ! [put, Key, Value],
     receive
-        ok ->
-            ok;
-        enotfound ->
-            enotfound;
-        einval ->
-            einval;
-        _ ->
-            error   
-    end
+        ok -> ok;
+        _ -> error
+    end.
 
-del(ClientPid, K) ->
-    ClientPid ! {del, K, self()},
+get(ClientPid, Key) ->
+    ClientPid ! [get, Key],
     receive
-        ok ->
-            ok;
-        enotfound ->
-            enotfound;
-        einval ->
-            einval;
-        _ ->
-            error   
-    end
+        {ok, Value} -> {ok, Value};
+        _ -> error
+    end.
 
-get(ClientPid, K) ->
-    ClientPid ! {get, K, self()},
+del(ClientPid, Key) ->
+    ClientPid ! [del, Key],
     receive
-        {ok, K} ->
-            {ok, K};
-        enotfound ->
-            enotfound;
-        einval ->
-            einval;
-        _ ->
-            error   
-    end
+        {ok, Value} -> {ok, Value};
+        _ -> error
+    end.
 
 stats(ClientPid) ->
-    ClientPid ! {stats, self()},
+    ClientPid ! [stats],
     receive
-        {ok, } ->
-            ok;
-        enotfound ->
-            enotfound;
-        einval ->
-            einval;
-        _ ->
-            error   
-    end
+        {ok, List} -> {ok, List};
+        _ -> error
+    end.
+
---
+++ oldclient.erl
@@ -0,0 +1,147 @@
+-module(client).
+-export([start/1, text_client/1, bin_client/1]).
+-export([put/3, del/2, get/2, stats/1]).
+
+start(ServerSocket) ->
+    {ok, Socket} = gen_tcp:connect("localhost", ServerSocket, [{active, true}]),
+    case ServerSocket of
+        8888 ->
+            ClientPid = spawn(text_client(Socket));
+        8889 ->  
+            ClientPid = spawn(bin_client(Socket))
+    end,
+    {ok, ClientPid}.
+
+text_client(Socket) ->
+    receive
+        {put, K, V, Pid} ->
+            Msg = io_lib:format("PUT ~p ~p", [K, V]);
+        {del, K, Pid} ->
+            Msg = io_lib:format("DEL ~p", [K]);
+        {get, K, Pid} ->
+            Msg = io_lib:format("GET ~p", [K]);
+        {stats, Pid} ->
+            Msg = io_lib:format("STATS")
+    end,
+    gen_tcp:send(Socket, Msg),
+    receive 
+        {tcp, Socket, Reply} ->
+            Chunks = string:tokens(Msg, [$\s]),
+            case lists:head(Chunks) of
+                "OK" -> 
+                    case length(Chunks) of
+                        1 -> 
+                            PID ! ok;
+                        _ -> 
+                            PID ! {ok, lists:nthtail(1, Chunks)}
+                    end;
+                "ENOTFOUND" -> 
+                    PID ! enotfound;
+                "EINVAL" ->
+                    PID ! einval
+            end
+    end,
+    text_client(Socket).
+
+get_length(Length) ->
+    if
+        Length >= 256 ->
+            integer_to_list(get_length(Length div 256))++integer_to_list((Length rem 256));
+        true ->
+            Length
+    end.
+
+bin_client(Socket) ->
+    receive
+        {put, K, V, Pid} ->
+            Task = integer_to_list(11),
+            LengthKey = get_length(length(K)),
+            LengthValue = get_length(length(V)),
+            Message = list_to_binary(Task++LengthKey++K++LengthValue++V);
+        {del, K, Pid} ->
+            Task = integer_to_list(12),
+            LengthKey = get_length(length(K)),
+            Message = list_to_binary(Task++LengthKey++K);
+        {get, K, Pid} ->
+            Task = integer_to_list(13),
+            LengthKey = get_length(length(K)),
+            Message = list_to_binary(Task++LengthKey++K);
+        {stats, Pid} ->
+            Task = integer_to_list(21),
+            Message = list_to_binary(Task)
+    end,
+    gen_tcp:send(Socket, Message),
+    receive 
+        {tcp, Socket, Msg} ->
+            String = binary_to_list(Msg),
+            case lists:head(String) of
+                101 -> 
+                    case length(String) of
+                        1 -> PID ! ok;
+                        _ -> do_something
+                    end;
+                111 ->
+                    PID ! einval;
+                112 -> 
+                    PID ! enotfound;
+                113 ->
+                    PID ! ebinary;
+                114 ->
+                    PID ! ebig;
+                115 ->
+                    PID ! eunk
+            end
+    end
+    bin_client(Socket).
+
+put(ClientPid, K, V) ->
+    ClientPid ! {put, K, V, self()},
+    receive
+        ok ->
+            ok;
+        enotfound ->
+            enotfound;
+        einval ->
+            einval;
+        _ ->
+            error   
+    end.
+
+del(ClientPid, K) ->
+    ClientPid ! {del, K, self()},
+    receive
+        ok ->
+            ok;
+        enotfound ->
+            enotfound;
+        einval ->
+            einval;
+        _ ->
+            error   
+    end
+
+get(ClientPid, K) ->
+    ClientPid ! {get, K, self()},
+    receive
+        {ok, K} ->
+            {ok, K};
+        enotfound ->
+            enotfound;
+        einval ->
+            einval;
+        _ ->
+            error   
+    end
+
+stats(ClientPid) ->
+    ClientPid ! {stats, self()},
+    receive
+        {ok, } ->
+            ok;
+        enotfound ->
+            enotfound;
+        einval ->
+            einval;
+        _ ->
+            error   
+    end
